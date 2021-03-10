ab03a8e9db6ab613e40d8210102112144b9c2a6a
RTBlanco <ronnytoribio1@hotmail.com>
2021-03-09 21:15:54 -0500

    commit tree 2c5f67bcaabb33b329ec7cfecf55268e37be3851

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new 
+  end 
+  
+  def create 
+    return redirect_to if params[:name].nil? || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to '/'
+  end 
+
+  def destroy
+    session.clear
+  end 
+end
